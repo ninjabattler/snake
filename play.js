c8bf0100254041d87e295e8a5b8b093bbc1df29c
ninjabattler <jaydenrtucker@gmail.com>
2020-08-26 20:14:47 +0000

    Added a function, setupInput, that sets up a stdin to read user input. Also added a function, handleUserInput, to control what happens with each input

--- play.js
+++ play.js
@@ -1,8 +1,26 @@
 const connect = require('./client');
-/**
- * Establishes connection with the game server
- */
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key)=>{
+
+    handleUserInput(key)
+
+  });
+  return stdin;
+}
+
+const handleUserInput = (key) => {
+
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+};
 
 console.log('Connecting ...');
+setupInput();
 connect();
